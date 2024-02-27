452967fe189cf4a3244f3a38377cc981c8f3f6c7
lohikrme <chinese.parrotlet@gmail.com>
2024-02-27 10:07:32 +0200

    finetuned functions and descriptions of estimations.R, fixed errors

--- estimations.R
+++ estimations.R
@@ -1,19 +1,28 @@
 # estimations
 # 13.2.2024
 
+# Terminologiasta lyhyesti: 
+# Keskivirhe(SE) on keskihajonta(sd) / sqrt(otoskoko)
+# Luottamusväli on ääriarvosta ääriarvoon
+# Kun taas margin_of_error(E) on puolet luottamusvälistä
+# eli jos luottamusväli = 1.5mm, niin silloin E=0.75mm
+# Funktioissa kutsun E:tä usein "luottamusvälin puolikkaaksi"
+# englanniksi luottamusvälin_puolikas on siis margin_of_error.
+
 options(digits = 10)
 
 getwd()
 setwd("C:\\Users\\Parrot\\data-analyysi1\\visualisointi\\Rstudio-graphs\\")
 getwd()
 
+
+# Tehtävä 1A: Laske keskiarvo ja hajonta, 
+# anna vastaus 2 desimaalin tarkkuudella:
+
 dataset1 = c(20.0, 18.6, 18.9, 19.3, 19.5, 19.2, 20.1, 20.2, 19.4, 18.8, 18.2, 
              19.4, 19.6, 20.1, 19.3, 18.7, 20.3, 19.5, 19.6, 19.7, 20.5, 18.6, 
              18.9, 19.3, 19.5, 19.2, 20.1, 20.2, 19.4, 18.8, 18.2, 19.4, 19.6, 
              20.1, 19.3, 18.7, 20.3, 19.5, 19.6, 19.7)
-
-# laske keskiarvo, hajonta
-# anna vastaus kahden desimaalin tarkkuudella
 
 mean1 = round(mean(dataset1), 2)
 mean1
@@ -24,7 +33,7 @@ sd1
 sample_size1 = length(dataset1)
 sample_size1
 
-# laske 99% luottamusrajat samalle datasetille
+# Tehtävä 1B: laske 99% luottamusrajat samalle datasetille
 # koska otoskoko on yli 30, mutta populaation keskihajonta on tuntematon
 # käytetään studentin t-jakaumaa
 # HUOM: R:ssä qt-funktio on oletusarvona 1-suuntainen
@@ -33,27 +42,26 @@ sample_size1
 # kun df=9 ja siten otoskoko = 9+1
 # jos tehdään 2-suuntainen testi, pitäisi jakaa häntä 2:lla
 
-conf_inter = function (keskiarvo, keskihajonta, luottamusvali, otoskoko) {
-  percent = 1 - (1 - luottamusvali/100) / 2
-  print(paste("percent: ", percent))
-  T =  qt(percent, df = otoskoko - 1)
-  print(paste("T-value:", T))
-  print(paste("population SD:", keskihajonta))
-  print(paste("sample SD:", keskihajonta / sqrt(otoskoko)))
-  print(paste("standard error:", T * (keskihajonta / sqrt(otoskoko))))
-  # mene T * keskihajontaa vasemmalle ja oikealle keskiarvosta
-  pienin = round(keskiarvo - T * keskihajonta / sqrt(otoskoko), 2)
-  suurin = round(keskiarvo + T * keskihajonta / sqrt(otoskoko), 2)
-  
-  return (c(pienin, suurin))
+luottamusvali_funktio = function (keskiarvo, keskihajonta, luottamusprosentti, otoskoko) {
+  merkitsevyystaso = 1 - (1 - luottamusprosentti/100) / 2
+  T =  qt(merkitsevyystaso, df = otoskoko - 1)
+  keskivirhe = keskihajonta / sqrt(otoskoko) # keskihajonta pitää sisällään "tutkittavan yksikön"
+  luottamusvalin_puolikas = T * keskivirhe
+  # mene luottamusvälin puolikkaan verran vasemmalle ja oikealle keskiarvosta
+  pienin = round(keskiarvo - luottamusvalin_puolikas, 2)
+  suurin = round(keskiarvo + luottamusvalin_puolikas, 2)
+  formatted_pienin = sprintf("%.2f", pienin)
+  formatted_suurin = sprintf("%.2f", suurin)
+  return (paste("pienin:", formatted_pienin, "... suurin:", formatted_suurin))
 }
 
-conf_inter(mean1, sd1, 99, sample_size1)
+luottamusvali_funktio(mean1, sd1, 99, sample_size1)
 
 
 
 
-# kyselytutkimuksessa kysytään 2300 ihmiseltä mielipide
+# Tehtävä 2: kyselytutkimuksessa kysytään 
+# 2300 ihmiseltä mielipide
 # 628 kannatti poliitikon ajamaa asiaa
 # poliitikko sanoo, että 30 prosenttia kansasta kannattaa
 # määritä otoksesta 95 ja 99% luottamusvälit, ja sen perusteella
@@ -72,36 +80,31 @@ conf_inter(mean1, sd1, 99, sample_size1)
 # eli lopullinen lasku on:
 # sqrt(p*q) / sqrt(n)
 
-options(digits = 10)
 
 sample_size = 2300
 support = 628/2300
 
 
-conf_interv_percents = function (support, sample_size, confidence_interval) {
-  print(paste("support", support))
-  print(paste("opposition", 1-support))
-  print(paste("sample_size", sample_size))
-  sd = sqrt(support*(1-support))
-  print(paste("sd", sd))
-  sample_sd = sd / sqrt(sample_size)
-  print(paste("sample_sd", sample_sd))
-  how_many_sd_from_avg = qnorm(1-(1-confidence_interval/100)/2)
-  print(paste("how_many_sd_from_avg", how_many_sd_from_avg))
-  standard_error = sample_sd * how_many_sd_from_avg
-  print(paste("standard_error", standard_error))
-  
-  # laske luottamusväli ja palauta se
-  lower = round(support - standard_error, 3)
-  higher = round(support + standard_error, 3)
-  return (c(lower, higher))
+luottamusvali_prosentit = function (kannatus, luottamusprosentti, otoskoko) {
+  sd = sqrt(kannatus*(1-kannatus))
+  merkitsevyystaso = 1-(1-luottamusprosentti/100)/2
+  keskivirhe = sd / sqrt(otoskoko)
+  Z = qnorm(merkitsevyystaso)
+  luottamusvalin_puolikas = keskivirhe * Z
+  # laske luottamusrajat ja palauta se
+  pienin = round(kannatus - luottamusvalin_puolikas, 3)
+  suurin = round(kannatus + luottamusvalin_puolikas, 3)
+  formatted_pienin = sprintf("%.3f", pienin)
+  formatted_suurin = sprintf("%.3f", suurin)
+  return (paste("pienin:", formatted_pienin, "... suurin:", formatted_suurin))
 }
 
-conf_interv_percents(support, sample_size, 99)
+luottamusvali_prosentit(support, 95, sample_size)
+luottamusvali_prosentit(support, 99, sample_size)
 
 
 
-# tehdas valmistaa ruuveja. ruuvien paino
+# Tehtävä 3: tehdas valmistaa ruuveja. ruuvien paino
 # vaihtelee satunnaisesti normaalijakautuneesti
 # otoskeskiarvo 18g. oletetaan (epärealistisesti)
 # että normaalijakauman varianssi 0.16 g^2.
@@ -112,68 +115,82 @@ conf_interv_percents(support, sample_size, 99)
 # vastaa sitä. Koska normaalijakauman varianssi ja 
 # siten myös keskihajonta ovat tunnettuja
 # Pienemmässä otos on alle 30, eli käytän T-testiä
-# isommassa otos on yli 30 ja popkeskihajonta tunnettu eli Z-testi'
+# isommassa otos on yli 30 ja käytän Z-testiä
 
 # keskivirhe eli keskihajontojen pituus saadaan
 # varianssista. pitää vain ottaa huomioon otoskoko
 
-options(digits = 10)
-
-Z = qnorm(0.975)
+# luottamusprosentti on 95%, eli 2.5% per häntä
+# Muuta varianssi sd:ksi
+luottamusprosentti = 95
 sd = sqrt(0.16)
 average = 18
+otoskoko1 = 23
+otoskoko2 = 2300
 
+# pienemmän otoksen lasken T-jakaumalla koska alle 30 otos:
 
-conf_Z = function (Z, sd, average, sample_size) {
-  sample_sd = sd/sqrt(sample_size)
-  low = signif(average - Z * sample_sd, 4)
-  high = signif(average + Z * sample_sd, 4)
-  return (c(low, high))
+conf_T = function (sd, keskiarvo, luottamusprosentti, otoskoko) {
+  luottamusdesimaali = luottamusprosentti / 100
+  kaksisuuntainen_luottamusvali = luottamusdesimaali + (1-luottamusdesimaali)/2
+  print(kaksisuuntainen_luottamusvali)
+  standard_error = sd/sqrt(otoskoko)
+  T = qt(kaksisuuntainen_luottamusvali, otoskoko - 1)
+  pienin = signif(keskiarvo - T * standard_error, 4)
+  suurin = signif(keskiarvo + T * standard_error, 4)
+  return (paste("pienin:", pienin, "... suurin:", suurin))
 }
 
-conf_Z(Z, sd, average, 2300)
 
-# pienemmän otoksen laskun lasken T-testillä koska alle 30 otos:
+# isomman otoksen lasken z-jakaumalla:
 
-conf_T = function (sd, average, sample_size) {
-  sample_sd = sd/sqrt(sample_size)
-  T = qt(0.975, sample_size - 1)
-  low = signif(average - T * sample_sd, 4)
-  high = signif(average + T * sample_sd, 4)
-  return (c(low, high))
+conf_Z = function (sd, keskiarvo, luottamusprosentti, otoskoko) {
+  luottamusdesimaali = luottamusprosentti / 100
+  kaksisuuntainen_luottamusvali = luottamusdesimaali + (1-luottamusdesimaali)/2
+  standard_error = sd/sqrt(otoskoko)
+  Z = qnorm(kaksisuuntainen_luottamusvali)
+  pienin = signif(keskiarvo - Z * standard_error, 4)
+  suurin = signif(keskiarvo + Z * standard_error, 4)
+  return (paste("pienin:", pienin, "... suurin:", suurin))
 }
 
-conf_T(sd, average, 23)
+# tulokset:
+conf_T(sd, average, 95, otoskoko1)
+conf_Z(sd, average, 95, otoskoko2)
 
 
 
-# normaalisti jakautuneesta suureesta 30kpl otos
-# otoksen keskiarvo 12.4, otoksen hajonta 1.4
+# Tehtävä 4A: normaalisti jakautuneesta suureesta 
+# otettiin 30kpl otos, jonka
+# otoskeskiarvo 12.4 ja otoshajonta 1.4
+# Laske 99% luottamusväli perusjoukon keskiarvolle.
 
-# ensin lasken 99% luottamusvälin perusjoukon keskiarvolle
-# koska ei tunneta pop keskihajontaa, käytän T-testiä
+# Koska ei tunneta pop keskihajontaa, käytän T-testiä
 
-options(digits = 10)
 
 sample_size = 30
 sd = 1.4
-sample_sd = sd/sqrt(sample_size)
+standard_error = sd/sqrt(sample_size)
 mean = 12.4
 T = qt(0.995, df = sample_size - 1)
-lower = round(mean - T * sample_sd, 2)
-higher = round(mean + T * sample_sd, 2)
-print(paste("lower", lower, "higher", higher))
+lower = mean - T * standard_error
+higher = mean + T * standard_error
+formatted_lower = sprintf("%.2f", lower)
+formatted_higher = sprintf("%.2f", higher)
+print(paste("lower", formatted_lower, "higher", formatted_higher))
 
 
-# sitten lasken, kuinka iso otoskoko pitäisi olla, jotta
+# Tehtävä 4b: Laske, kuinka iso otoskoko pitäisi olla, jotta
 # luottamusväli 99.9% olisi keskiarvo +- 0.1mm
-# tehtävässä sanotaan, että perusjoukon hajotna olisi 1.4mm
-# mikä mahdollistaa laskea, otoshajonnalla ei voisi laskea
+# tehtävässä sanotaan, että perusjoukon hajonta on 1.4mm
+# mikä mahdollistaa laskea, otoskeskihajonnalla ei voisi laskea
 
-# kaavana toimii: E = Z * pikku_sigma/sqrt(n).
-# E tarkoittaa siis keskivirhettä, pituutta keskiarvosta reunaan
-# tapauksessamme haluamme, että E = 0.1mm
-# mutta tähän tarkoitukseen se käännetään muotoon:
+# kaavana käytän: E = Z * pikku_sigma/sqrt(n).
+# E tarkoittaa margin_of_error pituutta
+# eli luottamusvälin puolikkaan pituutta
+# jos luottamusväli olisi 10, niin E olisi 5.
+# tehtävässä haluamme, että E = 0.1mm
+# saamme tämän helposti laskettua kaavalla:
 # n = ((Z * pikku_sigma)/E)^2
 
 
@@ -183,7 +200,7 @@ n = ceiling(((Z * pikku_sigma)/0.1)^2)
 n
 
 
-# galluppiin osallistui 3000 haastateltavaa
+# Tehtävä 5: Galluppiin osallistui 3000 haastateltavaa
 # 2115 vastasi kyllä
 # laske kyllä-vastanneiden prosenttiosuuden 
 # 95% luottamusrajat eli virhemarginaali
@@ -194,28 +211,27 @@ n
 # tulee sqrt(p*q) / sqrt(3000)
 # sen jälkeen käytetään Z-taulukkoa 95%
 
-options(digits = 10)
 
-conf_inter = function (support, sample_size, conf_interval) {
-  p = support/sample_size
+luottamusvali_funktio = function (kannatus, luottamusprosentti, otoskoko) {
+  p = kannatus/otoskoko
   q = 1 - p
-  sample_sd = sqrt(p*q) / sqrt(sample_size)
-  tail = (1-conf_interval/100)/2
+  standard_error = sqrt(p*q) / sqrt(otoskoko)
+  tail = (1-luottamusprosentti/100)/2
   Z = qnorm(1-tail)
-  standard_error = Z * sample_sd
+  luottamusvalin_puolikas = Z * standard_error
   mean = p
-  lower = round((mean - standard_error) * 100, 1)
-  higher = round((mean + standard_error) * 100, 1)
+  lower = round((mean - luottamusvalin_puolikas) * 100, 1)
+  higher = round((mean + luottamusvalin_puolikas) * 100, 1)
   return (c(lower, higher))
   
 }
 
-conf_inter(2115, 3000, 95)
+luottamusvali_funktio(2115, 95, 3000)
 
 
 
-# estimoitaessa normaalisti N(μ ; 2.2) jakautuneen
-# sarunnaisuureen odotusarvoa μ, otetaan n kpl otos.
+# Tehtävä 6: estimoitaessa normaalisti jakautuneen 
+# N(μ ; 2.2) satunnaisuureen odotusarvoa μ, otetaan n kpl otos.
 # Kuinka suuri otos on valittava, että μ:n
 # 99%:n luottamusvälin pituus ei ole suurempi kuin 1.5
 
@@ -223,18 +239,18 @@ conf_inter(2115, 3000, 95)
 # normaalijakautunutta dataa, jossa odotusarvo on tuntematon
 # ja jossa keskihajonta pikku_sigma on 2.2
 
-# Tässä kysytään luottamusvälin pituutta, eli käytetään kaavaa
+# Tehtävässä kysytään luottamusvälin pituutta.
+# Se saadaan helposti laskettua kaavasta
 # E = Z * pikku_sigma/sqrt(n)
-# jossa E tarkoittaa virhemarginaalin pituutta
-# virhemarginaalia voisi kutsua myös hännäksi, koska
-# E on luottamusvälin puolikkaan pituus
-# Siis jos on vaikka keskiarvo 100, SD 2, luottamusväli 95%
-# Niin silloin E = Z(0.975) * 2 = 1.96 * 2 = 3.92
-# Eli kun tehtävänannossa sanotaan, luottamusvälin pituus pitää olla
-# 1.5mm, se tarkoittaa, että E eli virhemarginaali pitää olla 
-# 1.5mm / 2 = 0.75mm suuntaansa
+# jossa E tarkoittaa luottamusvälin_puolikkaan pituutta.
+# Koska tehtävänannossa sanotaan 
+# luottamusvälin olevan on 1.5mm, 
+# puolikas on tietty 0.75mm.
+# Eli E = 1.5/2
 
-# tämän voi ratkaista kaavalla: n = ((Z * pikku_sigma)/E)^2 lasketaan
+# Tämän jälkeen sijoitetaan E kaavaan:
+# n = ((Z * pikku_sigma)/E)^2 
+# joka on johdettu samasta kaavasta ja kertoo tarvittavan otoskoon
 
 
 Z = qnorm(0.995) 
@@ -246,7 +262,7 @@ print(paste("Sample size:", n))
 
 
 
-# Heikki mittasi lepopulssiaan ja sai seuraavan otoksen:
+# Tehtävä 7: Heikki mittasi lepopulssiaan ja sai seuraavan otoksen:
 # 67, 62, 58, 74, 65, 66, 63
 # määritä 95% luottamusväli Heikin 
 # keskimääräiselle lepopulssille
@@ -256,17 +272,17 @@ dataset = c(67, 62, 58, 74, 65, 66, 63)
 mean = mean(dataset)
 sample_size = length(dataset)
 sd = sd(dataset)
-sample_sd = sd / sqrt(sample_size)
+standard_error = sd / sqrt(sample_size)
 T = qt(0.975, df = sample_size - 1)
 
-lower = round(mean - T * sample_sd, 1)
-higher = round(mean + T * sample_sd, 1)
+lower = round(mean - T * standard_error, 1)
+higher = round(mean + T * standard_error, 1)
 
 print(paste("Lower:", lower, "Higher:", higher))
 
 
 
-# Internetgallupissa 1500 suomalaiselta kysyttiin, 
+# Tehtävä 8: Internetgallupissa 1500 suomalaiselta kysyttiin, 
 # onko heillä ilmalämpöpumppu. 52.9% sanoi omistavansa.
 # Määritä 95% luottamusväli ilmalämpöpumpun omistavien
 # suhteelliselle osuudelle. 
@@ -277,11 +293,11 @@ owners = 0.529
 q = 1 - owners
 Z = qnorm(0.975)
 sd = sqrt(owners * q)
-sample_sd = sd / sqrt(sample_size)
+standard_error = sd / sqrt(sample_size)
 mean = owners
 
-lower = round((mean - Z * sample_sd)*100, 1)
-higher = round((mean + Z * sample_sd)*100, 1)
+lower = round((mean - Z * standard_error)*100, 1)
+higher = round((mean + Z * standard_error)*100, 1)
 
 print(paste("Lower:", lower, "Higher:", higher))
 
@@ -292,6 +308,9 @@ testdata = c(20.0, 18.6, 18.9, 19.3, 19.5, 19.2, 20.1, 20.2, 19.4, 18.8, 18.2,
              19.4, 19.6, 20.1, 19.3, 18.7, 20.3, 19.5, 19.6, 19.7, 20.5, 18.6, 
              18.9, 19.3, 19.5, 19.2, 20.1, 20.2, 19.4, 18.8, 18.2, 19.4, 19.6, 
              20.1, 19.3, 18.7, 20.3, 19.5, 19.6, 19.7)
+
+#----------------------------------------------------
+# Lopuksi: testailin vähän eroja miten sd:tä voi laskea:
 
 # divide with n-1
 mean = mean(testdata)
