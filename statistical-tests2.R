b850c6005cb4d0e4dc2dcfb2a47b1bdb96e51c05
lohikrme <chinese.parrotlet@gmail.com>
2024-02-28 11:32:53 +0200

    completed chi and paired t tests

--- statistical-tests2.R
+++ statistical-tests2.R
@@ -40,6 +40,7 @@ jalkeen = c(1.65, 2.00, 2.39, 1.62, 1.70,
 
 # Seuraavaksi käytän valmista funktiota:
 # funktio katsoo hypoteesin vasemmalta oikealle, eri järjestyksessä pitää olla tarkka.
+# Nyt kun on jalkeen, ennen, greater, se tarkoittaa, onko eka data suuurempi kuin toka data.
 t.test(jalkeen, ennen, alternative="greater", conf.level = 0.99, var.equal = TRUE, paired = TRUE)
 
 # saatu tulos: "t = 2.8017283, df = 19, p-value = 0.005690368"
@@ -101,3 +102,136 @@ format(vastaus2$p.value, scientific = FALSE)
 # jonka seurauksena
 # H0 hylätään, ja H1 astuu voimaan
 # Lääkkeellä on selvä vaikutus paranemiseen
+
+
+
+# Tehtävä 1: Testaa 5% riskillä, noudattavatko
+# linja-autojen kulkuajat tasaista jakaumaa.
+# Tätä varten laskettiin tunnin aikana
+# pysäkin ohittavat linja-autot. Mikä on testiin 
+# liittyvä p-arvo 5 desimaalin tarkkuudella?
+# Mikä on johtopäätös, kun
+# H0 = ei poikkea tilastollisesti 5% riskillä.
+# H1 = poikkeaa tilastollisesti 5% riskillä.
+# Dataksi saatiin seuraava (1,2,3,4) neljännestunti:
+data = c(6, 15, 9, 18)
+
+# koska data on luokitteluasteikollista, käytän
+# chi squared yhteensopivuustestiä:
+tn_pitaisi_olla = c(0.25, 0.25, 0.25, 0.25)
+
+vastaus = chisq.test(data, p = tn_pitaisi_olla)
+p_arvo = vastaus$p.value
+formatted_p_arvo = sprintf("%.5f", p_arvo)
+formatted_p_arvo
+
+# koska saatu p-arvo on 0.05756 eli yli 0.05,
+# H0 jää voimaan
+
+
+# Tehtävä 2: Alkoholin vaikutusta testattiin suorituskykyyn.
+# Testaa 5% riskitasolla, onko alkoholin nautiskelu
+# madaltanut testeistä saatavia pisteitä. Pistemäärät
+# oletetaan normaalisti jakautuneeksi.
+# Datana toimii seuraava:
+ennen = c(18.0,	17.0,	16.5,	15.5,	14.7,	14.0,	13.5,	12.5,	12.5,	11.5,	10.0,	9.0,	8.5,	8.2,	3.5)
+jalkeen = c(19.5,	18.5,	12.5,	16.4,	13.8,	13.5,	12.5,	9.0,	14.5,	8.0,	7.5,	6.0,	8.0,	7.8,	2.5)
+
+summary(ennen)
+summary(jalkeen)
+
+# Hypoteesit:
+# H0: jalkeen = ennen
+# H1: jalkeen < ennen
+
+# Koska data on normaalisti jakautunutta jatkuvaa dataa,
+# käytän riippuvaa t-testiä. 
+# Annan parametriksi myös var.equal, eli oletan molempien datojen varianssien olevan sama,
+# Laitan jalkeen, ennen, less, koska haluan katsoa, onko jalkeen pienempi kuin ennen.
+vastaus = t.test(jalkeen, ennen, alternative="less", conf.level = 0.95, var.equal = TRUE, paired = TRUE)
+
+p_value = vastaus$p.value
+formatted_p_value = sprintf("%.5f", p_value)
+formatted_p_value
+
+# Koska p-arvo 0.03486 on pienempi kuin 0.05, 
+# H1-hypoteesi astuu voimaan
+
+
+
+# Tehtävä 3: Tutkittiin rautatablettien vaikutusta
+# henkilöiden hemoglobiiniiniarvoihin. 
+# Rautatabletteja syötiin 3kk, ja hemoglobiini
+# mitattiin ennen ja jälkeen. 
+# Ilmoita p-arvo 6 desimaalin tarkkuudella.
+# Lisäksi testaa 1% riskillä, muuttiko 
+# rautatabletit hemoglobiinia johonkin suuntaan.
+# Dataksi saatiin seuraava: 
+ennen = c(136,	129,	140,	137,	128,	145,	156,	141,	133,	126,	122,	147,	138,	130,	154)
+jalkeen = c(142,	128,	145,	138,	134,	152,	165,	145,	141,	134,	128,	149,	136,	135,	154)
+
+# Käytän taas kerran riippuvaa (PAIRED=TRUE) t-testiä, koska data on jatkuvaa,
+# ja koska hemoglobiini on ilmiönä normaalijakautunut.
+vastaus = t.test(jalkeen, ennen, alternative="two.sided", conf.level = 0.99, var.equal=TRUE, paired=TRUE)
+p_value = vastaus$p.value
+formatted_p_value = sprintf("%.6f", p_value)
+formatted_p_value
+
+# Hypoteesit oli 1 % (0.01) riskitasolla:
+# H0: tablettien syöminen ei vaikuta hemoglobiiniin
+# H1: tablettien syöminen vaikuttaa hemoglobiiniin
+
+# Koska saatu p-arvo 0.000306 on pienempi kuin 0.01, 
+# H1 hypoteesi astuu voimaan riskitasolla 1%
+
+
+
+# Tehtävä 4: 80 koehenkilölle annetaan testiin 4 eri shampoota.
+# Jos jokainen shampoo olisi yhtä hyvä, 
+# Vastaukset jakautuisivat Shampoo (A, B, C, D) = (20, 20, 20, 20)
+# Testaa 5% riskillä, voidaanko shampoo-esimerkissä 
+# käyttää tasajakaumaa matemaattisena mallina
+# Ilmoita testistä saatu p-arvo 4 desimaalin tarkkuudella. 
+# Sekä päätä kumpi hypoteeseista on 5% riskillä tosi:
+# H0: shampoolaatujen välillä ei ole eroa
+# H1: shampoolaatujen välillä on eroa
+# Dataksi saatiin seuraava (suosikki shampoo) (A, B, C, D):
+data = c(15, 28, 17, 20)
+
+# koska data on luokitteluasteikollista, 
+# käytän chi squared yhteensopivuustestiä
+tn_pitaisi_olla = c(0.25, 0.25, 0.25, 0.25)
+
+vastaus = chisq.test(data, p = tn_pitaisi_olla)
+p_arvo = vastaus$p.value
+formatted_p_arvo = sprintf("%.4f", p_arvo)
+formatted_p_arvo
+
+# koska saatu p-arvo 0.1793 on suurempi kuin 0.05,
+# jää voimaan H0 hypoteesi
+
+
+# Tehtävä 5: Tutkittiin rokotteen tehoa.
+# Rokotettiin 70 vapaaehtoista.
+# Vertailuryhmäksi valittiin 70 rokottamatonta.
+# Ilmoita testistä saatu p-arvo 5 desimaalin tarkkuudella.
+# Testaa 1% riskillä, ehkäisikö rokote tartuntoja.
+# Dataksi saatiin c(sairastui, ei-sairastui) seuraavat tulokset:
+rokotetuista = c(20, 50)
+rokottamattomista = c(40, 30)
+
+# Data on luokitteluasteikollista, eli käytän
+# chi squared riippumattomuustestiä:
+
+# Riskitaso 1 % (0.01)
+# H0: Rokote ei vaikuttanut tartuntoihin
+# H1: Rokotteella on ehkäisevää vaikutusta tartuntoihin'
+matriisi = matrix(c(rokotetuista, rokottamattomista), nrow=2, byrow = TRUE)
+
+vastaus = chisq.test(matriisi)
+p_arvo = vastaus$p.value
+formatted_p_arvo = sprintf("%.5f", p_arvo)
+formatted_p_arvo
+
+# saatu p-arvo 0.00118 on pienempi kuin 0.01
+# Eli H1 hypoteesi astuu voimaan
